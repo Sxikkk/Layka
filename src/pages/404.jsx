4292fadf66fae795fe1bda1a225f596c89066a72
Sxikkk <wallker120@gmail.com>
2024-12-25 09:25:34 +0500

    deploy test

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,9 +2,13 @@ import {Link} from "react-router";
 
 const Error = () => {
     return (
-        <div>
+        <div style={{
+            display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: '3rem'
+        }}>
             <h1>ERROR 404</h1>
-            <h3>Page not found</h3>
+            <h3
+                style={{color: 'white', fontSize: '2rem'}}
+            >Page not found</h3>
             <Link to='/' key='404'>Back to main</Link>
         </div>
     );
